cfdd06ca52335e695225747f50d7c0004426046a
TaduJR <nattysec26@gmail.com>
2023-09-16 22:42:07 +0300

    FizzBuzz

---
+++ 1. FizzBuzz.js
@@ -0,0 +1,12 @@
+// Challange Link: https://leetcode.com/problems/fizz-buzz/
+
+const fizzBuzz = function(n) {
+    const result = [];
+    for(let i = 1; i <= n; i++) {
+        if(i % 3 == 0 && i % 5 == 0) result.push("FizzBuzz");  
+        else if(i % 3 == 0) result.push("Fizz");
+        else if(i % 5 == 0) result.push("Buzz");
+        else result.push(String(i));
+    }
+    return result;
+};
